c64e2b5774d4175fa53f0da34f086813f8054c50
Juliana <jzf0080@auburn.edu>
2023-02-02 23:40:43 -0600

    Background working

--- Assignment 3/ViewController.swift
+++ Assignment 3/ViewController.swift
@@ -12,8 +12,31 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        switch hour {
+        case 1...12:
+            background.image = UIImage(named:"RioDay")
+        case 13...24:
+                background.image = UIImage(named:"RioNight")
+        default:
+            background.image = UIImage(named:"RioDay")
+         }
     }
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var timePicker: UIDatePicker!
+    
+    @IBOutlet weak var label1: UILabel!
+    
+    let hour = Int(Calendar.current.component(.hour, from: Date()))
+    
+    
 
+    
+    
+    
+    @IBAction func timePicker(_ sender: UIDatePicker) {
+    }
+    @IBAction func timerButton(_ sender: UIButton) {
+    }
 }
 
