054d67d5ab35fa5693625eb5b7268e2f0fe12b0b
Juliana <jzf0080@auburn.edu>
2023-02-03 19:59:46 -0600

    Unsure

--- Assignment 3/ViewController.swift
+++ Assignment 3/ViewController.swift
@@ -8,35 +8,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let hour = Int(Calendar.current.component(.hour, from: Date()))
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label2.isHidden = true
         switch hour {
         case 1...12:
             background.image = UIImage(named:"RioDay")
         case 13...24:
-                background.image = UIImage(named:"RioNight")
+            background.image = UIImage(named:"RioNight")
         default:
             background.image = UIImage(named:"RioDay")
-         }
+        }
+        
+        
     }
-
+    var counter = 0
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var background: UIImageView!
     @IBOutlet weak var timePicker: UIDatePicker!
     
     @IBOutlet weak var label1: UILabel!
     
-    let hour = Int(Calendar.current.component(.hour, from: Date()))
-    
-    
-
-    
-    
     
     @IBAction func timePicker(_ sender: UIDatePicker) {
+        
     }
     @IBAction func timerButton(_ sender: UIButton) {
+        label2.isHidden = false
+        label2.text = "I told you I'd be back"
+        label1.text = "I changed!"
+        button1.setTitle("Changing too", for: .normal)
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds to the end of the world")
+            counter -= 1
+        }
     }
 }
-
