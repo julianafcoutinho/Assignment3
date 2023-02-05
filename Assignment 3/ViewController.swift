86fa22ea645082ed1ebf3c96b885e9007a65251f
Juliana <jzf0080@auburn.edu>
2023-02-04 21:02:36 -0600

    Good?

--- Assignment 3/ViewController.swift
+++ Assignment 3/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     var counter = 0
@@ -20,21 +21,30 @@ class ViewController: UIViewController {
     }
     
     var timer = Timer()
+    var countdown:Int = 0
+    var player: AVAudioPlayer!
+    var hours:Int = 0
+    var minutes: Int = 0
+    var seconds: Int = 0
+    var today = Date()
+    let format = DateFormatter()
+    var hour = Int(Calendar.current.component(.hour, from: Date()))
     
-    let hour = Int(Calendar.current.component(.hour, from: Date()))
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        //trying to make the text work for the button when the device is horizontal
+        button1.titleLabel?.adjustsFontSizeToFitWidth = true
+        button1.titleLabel?.minimumScaleFactor = 0.4
+        format.dateFormat = "EEE, dd MMM YYYY HH:mm:ss"
         label2.isHidden = true
-        label2.text = ""
         button1.setTitle("Start Timer", for: .normal)
-        let today = Date()
-        let hours = (Calendar.current.component(.hour, from: today))
-        let minutes = (Calendar.current.component(.minute, from: today))
-        let seconds = (Calendar.current.component(.second, from: today))
-        
-        label1.text = DateFormatter.localizedString(from: Date(), dateStyle: .long, timeStyle: .short)
+        label1.text = format.string(from:Date())
         timer = Timer.scheduledTimer(timeInterval:1.0, target: self, selector:#selector(self.tick), userInfo: nil, repeats: true)
+        //considered having this only inside the function, but there was an obvious delay
+        hour = Int(Calendar.current.component(.hour, from: Date()))
         switch hour {
         case 1...12:
             background.image = UIImage(named:"RioDay")
@@ -43,46 +53,90 @@ class ViewController: UIViewController {
         default:
             background.image = UIImage(named:"RioDay")
         }
-        
-        
+        startSound()
     }
 
-    
-   
+    //configures what happens whenever the button is pressed
     @IBAction func timerButton(_ sender: UIButton) {
-        
-        label2?.isHidden = false
-        label2?.text = "I told you I'd be back"
-        
-        let date = timePicker.date
-        let components = Calendar.current.dateComponents([.hour, .minute], from: date)
-        let hr = components.hour!
-        let min = components.minute!
-        
-        let countdown = hr*3600 + min*60
-        button1.setTitle("Wait for timer.", for: .normal)
-        //change button and play music when timer ends
-                    
-                
-                    
-                    
-                  //  timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(startCountDown) , userInfo: nil, repeats: true)
-                  //  buttonText.setTitle("Stop Music", for: .normal)
-       
-        
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
-        
-    }
-    @objc func updateCounter() {
-        //example functionality
-        if counter > 0 {
-            label2?.text = "seconds to the end of the world"
-            counter -= 1
+        //button behavior at the beginning of the program/whenever it's ready for a new timer
+        if(button1.currentTitle == "Start Timer") {
+            let date = timePicker.date
+            let components = Calendar.current.dateComponents([.hour, .minute], from: date)
+            let hr = components.hour!
+            let min = components.minute!
+            
+            countdown = hr*3600 + min*60
+            label2.isHidden = false
+            button1.setTitle("Want to reset?", for: .normal)
+            
+            timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(startTimer) , userInfo: nil, repeats: true)
+            startTimer()
+            // Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+            
         }
-       
+        //button behavior if a timer completed its run
+        else if (button1.currentTitle == "Stop Music") {
+            button1.setTitle("Start Timer", for: .normal)
+            stopSound()
+            label2.isHidden = true
+        }
+        //button behavior if a timer is currently running
+        else {
+            button1.setTitle("Start Timer", for: .normal)
+            timer.invalidate()
+            label2.isHidden = true
+            //stop timer
+        }
     }
-    @objc func tick() {
-        label1.text = DateFormatter.localizedString(from: Date(), dateStyle: .long, timeStyle: .short)
     
+    //to change the time on the top label and update the background based on the time
+    @objc func tick() {
+        hour = (Calendar.current.component(.hour, from: Date()))
+        label1.text = format.string(from:Date())
+        switch hour {
+        case 0...11:
+            background.image = UIImage(named:"RioDay")
+        case 12...24:
+            background.image = UIImage(named:"RioNight")
+        default:
+            background.image = UIImage(named:"RioDay")
+        }
+    }
+    
+    //starts the timer once the button is pressed, and updates the bottom label
+    @objc func startTimer() {
+        countdown -= 1
+        label2.text = "\(String(format: "%02d", countdown/3600)):\(String(format: "%02d",(countdown%3600)/60)):\(String(format: "%02d",(countdown%60))) seconds remaining"
+        if countdown == 0 {
+            playSound()
+            timer.invalidate()
+            label2.text = "Countdown has completed"
+            button1.setTitle("Stop Music", for: .normal)
+        }
+    }
+    
+    //sets up the plaer
+    @objc func startSound() {
+        guard let url = Bundle.main.url(forResource: "ComfortablyNumb", withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            player = try AVAudioPlayer(contentsOf: url)
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
+    }
+    
+    //plays sound
+    @objc func playSound() {
+        player?.play()
+    }
+    
+    //stops sound
+    @objc func stopSound() {
+        player?.stop()
     }
 }
